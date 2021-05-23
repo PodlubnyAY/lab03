be0d55a558e77abb899b92e5039b80dc674649a2
PodlubnyAY <PDLBN@gmail.com>
2021-05-23 17:09:21 +0300

    SVG

--- lb3/main.cpp
+++ lb3/main.cpp
@@ -94,6 +94,77 @@ show_histogram_text(const vector<size_t>& bins)
     return 0;
 }
 
+void
+svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void
+svg_end()
+{
+    cout << "</svg>\n";
+}
+
+void
+svg_text(double left, double baseline, string text)
+{
+
+    cout << "<text x='" << left << "' y='"<< baseline <<"'>"<< text <<"</text>\n";
+
+}
+
+void svg_rect(double x, double y, double width, double height)
+{
+    cout << "<rect x='"<< x <<"' y='"<< y <<"' width='"<< width <<"' height='"<< height <<"'/>\n";
+}
+
+void svg_rect(double x, double y, double width, double height, string stroke, string fill )
+{
+    cout << "<rect x='"<< x <<"' y='"<< y <<"' width='"<< width <<"' height='"<< height <<"' stroke='"<<stroke<<"' fill='"<< fill<<"'/>\n";
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins)
+{
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    const auto MAX_COUNT = 34;
+
+    double top = 0;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    size_t count;
+    size_t max_count = bins[0];
+    for (size_t bin : bins)
+    {
+        if (bin > max_count)
+        {
+            max_count = bin;
+        }
+    }
+
+        for (size_t bin : bins)
+        {
+            auto height = bin;
+            const double bin_width = BLOCK_WIDTH * height;
+            svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+            svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "blue", "#ff2233");
+            top += BIN_HEIGHT;
+
+        }
+        svg_end();
+}
+
 int
 main()
 {
@@ -111,7 +182,7 @@ main()
 
     const auto bins = make_histogram(numbers, bin_count);
 
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
 
     return 0;
 }
