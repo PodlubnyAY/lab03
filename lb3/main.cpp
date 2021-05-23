f12eeb0650281d6bcc736112483c68eb2606a897
PodlubnyAY <PDLBN@gmail.com>
2021-05-23 20:33:08 +0300

    Получение аргументов программы

--- lb3/main.cpp
+++ lb3/main.cpp
@@ -25,31 +25,42 @@ Input
 read_input(istream& in, bool prompt)
 {
     Input data;
-if (prompt){
-    cerr << "Enter number count: ";
-    size_t number_count;
-    in >> number_count;
+    if (prompt)
+    {
+        cerr << "Enter number count: ";
+        size_t number_count;
+        in >> number_count;
 
-    cerr << "Enter numbers: ";
-    data.numbers = input_numbers(in, number_count);
+        cerr << "Enter numbers: ";
+        data.numbers = input_numbers(in, number_count);
 
-    cerr << "Enter column count: ";
-    in >> data.bin_count;
-}
-else {
-    size_t number_count;
-    in >> number_count;
-    data.numbers = input_numbers(in, number_count);
-    in >> data.bin_count;
-}
+        cerr << "Enter column count: ";
+        in >> data.bin_count;
+    }
+    else
+    {
+        size_t number_count;
+        in >> number_count;
+        data.numbers = input_numbers(in, number_count);
+        in >> data.bin_count;
+    }
 
     return data;
 }
 
 
 int
-main()
+main(int argc, char* argv[])
 {
+    if (argc > 1)
+    {
+        cout << "argc = " << argc << '\n';
+        for (int i = 0; i < argc; i++)
+        {
+
+            cout << "argv[" << i << "] = " << argv[i] << '\n';
+        }
+    }
     curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
