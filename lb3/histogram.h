0c4d18701603afd343596694aa73201bd9b0bfd1
PodlubnyAY <PDLBN@gmail.com>
2021-05-24 01:48:29 +0300

    4.2

--- lb3/histogram.h
+++ lb3/histogram.h
@@ -5,6 +5,10 @@
 using namespace std;
 
 void find_minmax(const vector<double> numbers, double& min, double& max);
+void
+show_histogram_text(const vector<size_t>& bins);
+vector<double> input_numbers(size_t count);
+vector<size_t> make_histogram(vector <double> numbers, size_t bin_count);
 
 
 
