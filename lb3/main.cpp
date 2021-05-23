199d4f7e5085ec112bb01aa6531739ace0cd4198
PodlubnyAY <PDLBN@gmail.com>
2021-05-23 20:26:26 +0300

    Установка

--- lb3/main.cpp
+++ lb3/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "svg.h"
 #include "histogram.h"
+#include <curl/curl.h>
 
 
 using namespace std;
@@ -45,11 +46,14 @@ else {
     return data;
 }
 
+
 int
 main()
 {
+    curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
+
     return 0;
 }
